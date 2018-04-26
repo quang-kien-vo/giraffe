6c45d775e9c886082e093a7051743025b1da25b6
quang <quang.vo@autogravity.com>
2018-04-25 23:56:04 -0700

    working version with more than 2 children

--- graph/node.rb
+++ graph/node.rb
@@ -1,21 +1,14 @@
 class Node
-  attr_accessor :value, :left, :right, :name
+  attr_accessor :value, :children, :name
 
   def initialize(options={})
     @value = options[:value]
     @name = options[:name]
+    @children = []
   end
 
-  def children
-    [@left, @right].compact
-  end
-
-  def children?
-    @left && @right
-  end
-
-  def no_children?
-    !children?
+  def add_child(node)
+    @children.push(node)
   end
 end
 
@@ -25,11 +18,22 @@ child_2 = Node.new({:value => 3, :name => 'child_2'})
 grand_child_2 = Node.new({:value => 6, :name => 'grand_child_2'})
 grand_child_1 = Node.new({:value => 4, :name => 'grand_child_1'})
 grand_grand_child_1 = Node.new({:value => 5, :name => 'grand_grand_child_1'})
-grand_child_1.left = grand_grand_child_1
-child_1.left = grand_child_1
-child_1.right = grand_child_2
-root.left = child_1
-root.right = child_2
+
+root.add_child(child_1)
+root.add_child(child_2)
+child_1.add_child(grand_child_2)
+child_1.add_child(grand_child_1)
+grand_child_1.add_child(grand_grand_child_1)
+
+#
+#           (1)
+#          /   \
+#        (2)  (3)
+#       /  \
+#     (6) (4)
+#           \
+#          (5)
+#
 
 def bfs(node)
   queue = []
