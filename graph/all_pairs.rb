6e319f3a3cde66967afccf3a0d257c87ac6063ac
quang <quang.vo@autogravity.com>
2018-04-26 21:43:13 -0700

    find all paths from a node

--- graph/all_pairs.rb
+++ graph/all_pairs.rb
@@ -1,23 +1,30 @@
+require 'set'
+
 class Graph
   attr_accessor :graph
 
   def initialize
-    @graph = Hash.new { |hash, key| hash[key] = [] }
+    @graph = Hash.new {|hash, key| hash[key] = []}
   end
 
-  def add_edge(a, b)
-    @graph[a] << b
-    @graph[b] << a
+  def add_edge(source, destination)
+    @graph[source] << destination
+    @graph[destination] << source
   end
 
 
-  def find_paths(a, b, result, &bl)
-    result = result+[a] # !! copy and add a
+  def find_paths(source, destination, result, &bl)
+    result = result+[source] # !! copy and add a
 
-    bl.call(result) if a == b
-    @graph[a].each do |v|
-      find_paths(v, b, result, &bl) if ! result.include?(v)
+    #p result
+    bl.call(result) if source == destination
+    @graph[source].each do |v|
+      find_paths(v, destination, result, &bl) if !result.include?(v)
     end
+  end
+
+  def print_all_nodes
+    @graph.keys
   end
 
 end
@@ -40,7 +47,22 @@ g2.add_edge('b', 'e')
 g2.add_edge('b', 'f')
 g2.add_edge('f', 'g')
 g2.add_edge('c', 'f')
-p g2.graph
-g2.find_paths('a', 'f', []) { |path|
-  puts "path : #{path.join(", ")} / len=#{path.size}"
-}
+g2.add_edge('d', 'f')
+
+all_paths = []
+
+g2.find_paths('a', 'g', []) {|path| all_paths.push(path)}
+p all_paths
+
+p "--------------------------"
+
+def find_all_paths_from_root(graph)
+  graph.graph.keys.each do |iterator|
+
+    all_paths = []
+    graph.find_paths('a', iterator, []) {|path| all_paths.push(path)}
+    p all_paths
+  end
+end
+
+find_all_paths_from_root(g2)
